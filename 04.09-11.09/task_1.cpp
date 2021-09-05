6e8f873667956d8f32f40f4af0d147ac89d97661
123 <fomin.nv@phystech.edu>
2021-09-05 19:55:20 +0300

    temperature

--- 04.09-11.09/task_1.cpp
+++ 04.09-11.09/task_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 
 const int NUMBER_WIDTH = 8;
 const int LINE_WIDTH = 25;
@@ -26,11 +27,16 @@ int main() {
 	cout << name << '\n';
 
 	cout << left << setw(LINE_WIDTH - NUMBER_WIDTH) << setfill('.') << "Price:" << hex << right << setw(NUMBER_WIDTH) << setfill('0') << price << endl;
-	
+
 	cout.fill('.');
 	cout.setf(ios::boolalpha);
+	cout.unsetf(ios::hex);
+	cout.setf(ios::dec);
 
 	cout << left << setw(hasCashback ? LINE_WIDTH - 4 : LINE_WIDTH - 5) << "Has cash-back:" << hasCashback << endl;
 
+	cout.setf(ios::showpos);
+
+	cout << left << setw(LINE_WIDTH - (int)log10(temperature) - 2) << "Max temperature" << temperature;
 	return 0;
 }

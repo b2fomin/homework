cc3f7c7be069c566de9cbef8e200872f3b9e3ca3
123 <fomin.nv@phystech.edu>
2021-09-05 19:41:45 +0300

    cashback

--- 04.09-11.09/task_1.cpp
+++ 04.09-11.09/task_1.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <iomanip>
 
+const int NUMBER_WIDTH = 8;
+const int LINE_WIDTH = 25;
+
 int main() {
 
-    using namespace std;
+	using namespace std;
 
-    std::string name;
-    int price, temperature;
-    bool hasCashback;
+	std::string name;
+	int price, temperature;
+	bool hasCashback;
 
-    cout << "Product's name: ";
-    cin >> name;
-    cout << "Product's price: ";
-    cin >> price;
-    cout << "Is cash-back available for this product? (true/false) ";
-    cin >> boolalpha >> hasCashback;
-    cout << "Maximum storing temperature: ";
-    cin >> temperature;
+	cout << "Product's name: ";
+	cin >> name;
+	cout << "Product's price: ";
+	cin >> price;
+	cout << "Is cash-back available for this product? (true/false) ";
+	cin >> boolalpha >> hasCashback;
+	cout << "Maximum storing temperature: ";
+	cin >> temperature;
 
-    cout << '\n' << "*************** OUTPUT ***************" << "\n\n";
+	cout << '\n' << "*************** OUTPUT ***************" << "\n\n";
 
-    cout << name << '\n';
+	cout << name << '\n';
 
-    cout << left << setw(25) << setfill('.') << "Price:" << hex << right << setfill('0') << price;
-    return 0;
+	cout << left << setw(LINE_WIDTH - NUMBER_WIDTH) << setfill('.') << "Price:" << hex << right << setw(NUMBER_WIDTH) << setfill('0') << price << endl;
+	
+	cout.fill('.');
+	cout.setf(ios::boolalpha);
+
+	cout << left << setw(hasCashback ? LINE_WIDTH - 4 : LINE_WIDTH - 5) << "Has cash-back:" << hasCashback << endl;
+
+	return 0;
 }

8939c021c4ec33e93ee6c9d0069f86d98ba8d1b6
123 <fomin.nv@phystech.edu>
2021-09-15 08:17:57 +0300

    split text

--- 11.09-18.09/task_1.cpp
+++ 11.09-18.09/task_1.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+const char punctuation[8] = { '!',':',',','.',';','?','(',')' }; // if you need to support more punctuation marks, put it here
+
+template<typename T>
+bool is_found(const T* arr, int size, T elem)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == elem) return true;
+	}
+	return false;
+}
 
 int main()
 {
@@ -8,6 +21,27 @@ int main()
 	cout << "Enter text:" << endl;
 	string text;
 	getline(cin, text);
+	int len = text.length();
+	vector<string> split_text;
+	string current = "";
+
+	for (int i = 0; i < len; i++)
+	{
+
+		if (!is_found(punctuation, 8, text[i]) && text[i] != ' ')
+		{
+			current += text[i];
+			continue;
+		}
+
+		if (current != "") split_text.push_back(current);
+		text[i] != ' ' ? current = text[i] : current = "";
+	}
+	split_text.push_back(current);
+
+	for (int i = 0; i < split_text.size(); i++) {
+		cout << "\"" << split_text[i] << "\"" << "\t";
+	}
 
 	return 0;
 }

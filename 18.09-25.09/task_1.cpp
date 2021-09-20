958bda021aae78ba0fef2bbefbe5db7b389ef115
123 <fomin.nv@phystech.edu>
2021-09-20 22:57:48 +0300

    fib function

--- 18.09-25.09/task_1.cpp
+++ 18.09-25.09/task_1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int fib(int number)
+{
+	if (number == 1 || number == 2) return 1;
+	return fib(number - 1) + fib(number - 2);
+}
+
 int main()
 {
 	using namespace std;
@@ -7,6 +13,8 @@ int main()
 	cout << "Enter number of Fibonacci sequence: ";
 	int number;
 	cin >> number;
+
+	cout << endl << "Result is " << fib(number);
 	
 	return 0;
 }

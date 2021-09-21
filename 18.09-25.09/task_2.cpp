50de1416348e659fa67de8b66e7b249b6a25a2d0
123 <fomin.nv@phystech.edu>
2021-09-21 14:57:40 +0300

    merge function and array output

--- 18.09-25.09/task_2.cpp
+++ 18.09-25.09/task_2.cpp
@@ -3,20 +3,43 @@
 
 using namespace std;
 
+void switch_numbers(int& a, int& b)
+{
+	a += b;
+	b = a - b;
+	a -= b;
+}
+
+void merge(vector<int>& arr, int left, int middle, int right)
+{
+	int current_1 = left, current_2 = middle;
+	while (current_1 < current_2 && current_2 < right )
+	{
+		if (arr[current_2] < arr[current_1])
+		{
+			for (int i = middle; i > current_1; i--) switch_numbers(arr[i], arr[i - 1]);
+			middle++;
+			current_2++;
+		}
+		else current_1++;
+	}
+}
+
 int main()
 {
 	cout << "Put your array here (put any symbol is not a digit to finish):" << endl;
 	vector<int> arr(1);
 
 	int i = 0;
-	while (cin>>arr[i])
+	while (cin >> arr[i])
 	{
 		arr.push_back(0);
 		i++;
 	}
 	arr.pop_back();
 
-	for (int i = 0; i < arr.size(); i++) cout << arr[i]<<'\t';
+
+	for (int i = 0; i < arr.size(); i++) cout << arr[i] << '\t';
 
 	return 0;
 }

ed4634a2ad4a56ff9bd8a10448372a83af6d2824
123 <fomin.nv@phystech.edu>
2021-09-29 22:34:30 +0300

    logical operators and test program

--- 1 semester/25.09-02.10/task_1.cpp
+++ 1 semester/25.09-02.10/task_1.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 struct Date
 {
-	int year;
-	int month;
+private: const int month_duration[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
+public:
 	int day;
+	int month;
+	int year;
 
-	Date(int year,int month,int day)
+	Date(int day = 0, int month = 0, int year = 0)
 	{
 		this->year = year;
 		this->month = month;
@@ -28,11 +30,34 @@ struct Date
 		month = other.month;
 		day = other.day;
 	}
+
+	Date operator+(const Date& other)
+	{
+		Date new_date;
+		new_date.day = day + other.day;
+
+		int duration = month_duration[max(month, other.month)];
+		new_date.month = month + other.month + new_date.day / duration;
+		new_date.day %= duration;
+
+		new_date.year = year + other.year + new_date.month / 12;
+		new_date.month %= 12;
+		return new_date;
+	}
+
+	bool operator==(const Date& other) { return (year == other.year && month == other.month && day == other.day); };
+
+	bool operator<(const Date& other) { return (year < other.year || month < other.month || day < other.day); };
+
+	bool operator>(const Date& other) { return (year > other.year || month > other.month || day > other.day); };
 };
 
 int main()
 {
-	Date today{ 795,9,29 };
-	today.print();
+	Date today{ 29,9,2021 };
+	Date tomorrow{ 30,9,2021 };
+	Date yesterday{ 28,9,2021 };
+	(today + tomorrow).print();
+	cout << endl << boolalpha << (today < tomorrow) << " " << (today==today) << " " << (today<yesterday) << endl;
 	return 0;
 }

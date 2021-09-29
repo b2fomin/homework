3b50764aade901d8f42a6bbf38e564d7edade026
123 <fomin.nv@phystech.edu>
2021-09-29 21:38:02 +0300

    print

--- 1 semester/25.09-02.10/task_1.cpp
+++ 1 semester/25.09-02.10/task_1.cpp
@@ -16,9 +16,16 @@ struct Date
 		this->month = month;
 		this->day = day;
 	}
+
+	void print()
+	{
+		cout << setw(2) << day << '.' << setw(2) << setfill('0') << month << '.' << year % 100;
+	}
 };
 
 int main()
 {
+	Date today{ 795,9,29 };
+	today.print();
 	return 0;
 }

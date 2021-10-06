19182a27786ccdba39775aa52cb1abb592812881
123 <fomin.nv@phystech.edu>
2021-10-06 18:07:05 +0300

    .hpp file

---
+++ 1 semester/02.10-09.10/task_1/Date.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<iomanip>
+
+class Date
+{
+	const int month_duration[12];
+	int day;
+	int month;
+	int year;
+
+public:
+	Date();
+	
+	void set_day(int);
+	void set_month(int);
+	void set_year(int);
+
+	int get_day() const;
+	int get_month() const;
+	int get_year() const;
+
+	friend std::ostream operator<<(std::ostream&, const Date&);
+	friend std::istream operator>>(std::istream, const Date&);
+	
+	Date& operator=(const Date&);
+	Date operator+(const Date&);
+	bool operator==(const Date&);
+	bool operator<(const Date&);
+	bool operator>(const Date&);
+
+};

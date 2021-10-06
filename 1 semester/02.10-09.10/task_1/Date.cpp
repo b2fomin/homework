77ed035f97c3d42d7c5d56abb000ed79142a523d
123 <fomin.nv@phystech.edu>
2021-10-06 19:07:07 +0300

    operators << and >>

--- 1 semester/02.10-09.10/task_1/Date.cpp
+++ 1 semester/02.10-09.10/task_1/Date.cpp
@@ -1,8 +1,7 @@
 #include "Date.hpp"
 
 
-
-Date::Date(int year=0, int month=0, int day=0):month_duration{ 31,28,31,30,31,30,31,31,30,31,30,31 }
+Date::Date(int year = 1, int month = 1, int day = 1) :month_duration{ 31,28,31,30,31,30,31,31,30,31,30,31 }
 {
 	set_year(year);
 	set_month(month);
@@ -51,3 +50,36 @@ bool Date::operator==(const Date& other) { return (year == other.year && month =
 bool Date::operator<(const Date& other) { return (year < other.year || month < other.month || day < other.day); };
 
 bool Date::operator>(const Date& other) { return (year > other.year || month > other.month || day > other.day); };
+
+std::ostream& operator<<(std::ostream& out, const Date& date)
+{
+	out << std::setw(2) << date.day << '.' << std::setw(2) << std::setfill('0') << date.month << '.' << std::setw(2) << std::setfill('0');
+	if (date.year < 0) out << -date.year << " BC";
+	else out << date.year;
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, const Date& date)//input format is day.month.year
+{
+	std::string answer;
+	in >> answer;
+	int fields[3]{ date.day, date.month, date.year };
+
+	for (auto& field : fields)
+	{
+		std::string current = "";
+		
+		for (auto& elem : answer)
+		{
+			if (elem != '.') current += elem;
+			else
+			{
+				answer.erase(answer.begin() + current.size() + 1);
+				break;
+			}
+		}
+
+		field = std::stoi(current);
+	}
+	return in;
+}

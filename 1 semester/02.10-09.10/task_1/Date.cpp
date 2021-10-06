b60944033f73ad618597ebfa0f90f40e6122fa47
123 <fomin.nv@phystech.edu>
2021-10-06 18:32:41 +0300

    setters

--- 1 semester/02.10-09.10/task_1/Date.cpp
+++ 1 semester/02.10-09.10/task_1/Date.cpp
@@ -8,3 +8,15 @@ Date::Date(int year=0, int month=0, int day=0):month_duration{ 31,28,31,30,31,30
 	set_month(month);
 	set_day(day);
 }
+
+void Date::set_year(int year) { this->year = year; };//date may be BC
+void Date::set_month(int month)
+{
+	if (0 < month <= 12) this->month = month;
+	else throw "Invalid month number";
+}
+void Date::set_day(int day)
+{
+	if (0 < day <= month_duration[month]) this->day = day;
+	else throw "Invalid day number";
+}

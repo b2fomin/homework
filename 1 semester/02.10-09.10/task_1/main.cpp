88f438fc72f440a30572a139095a19d227654137
123 <fomin.nv@phystech.edu>
2021-10-06 19:21:12 +0300

    testing program

---
+++ 1 semester/02.10-09.10/task_1/main.cpp
@@ -0,0 +1,15 @@
+#include "Date.hpp"
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+	using namespace std;
+	Date today{ 29,9,2021 };
+	Date tomorrow{ 30,9,2021 };
+	Date yesterday{ 28,9,2021 };
+	cout << today << " " << tomorrow << endl;
+	cout << (today + tomorrow);
+	cout << endl << boolalpha << (today < tomorrow) << " " << (today == today) << " " << (today < yesterday) << endl;
+	return 0;
+}

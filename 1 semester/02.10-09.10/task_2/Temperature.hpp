8df7df2493ae3af5662b9505a0b7110668ea6556
123 <fomin.nv@phystech.edu>
2021-10-07 03:48:51 +0300

    .hpp file

---
+++ 1 semester/02.10-09.10/task_2/Temperature.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+#include<functional>
+
+typedef std::function<double(double)> converter;
+
+class Temperature
+{
+	double value;
+	const struct Coefficients
+	{
+		converter Kelvin;
+		converter Fahrenheit;
+		converter Reamur;
+	};
+public:
+	static Coefficients coefficients;
+	void set_value(double, converter);
+	double get_value(converter) const;
+	Temperature(double);
+};

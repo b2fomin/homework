416d16294c4dbc72ace0f8a6da12d3fdab063fef
123 <fomin.nv@phystech.edu>
2021-10-13 21:40:55 +0300

    move constructor

--- 1 semester/09.10-16.10/task_1/Array.cpp
+++ 1 semester/09.10-16.10/task_1/Array.cpp
@@ -16,3 +16,8 @@ Array::Array(const Array& other) :size(other.size), arr(other.arr)
 	data = new int[size];
 	for (int i = 0; i < size; ++i) data[i] = other.data[i];
 }
+
+Array::Array(Array&& other) :size(other.size), arr(other.arr), data(other.data)
+{
+	other.data = nullptr;
+}

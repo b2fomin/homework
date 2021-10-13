b537ab5dfa0e67958b561b857ceaded0514a42b3
123 <fomin.nv@phystech.edu>
2021-10-13 21:34:06 +0300

    copy constructor

--- 1 semester/09.10-16.10/task_1/Array.cpp
+++ 1 semester/09.10-16.10/task_1/Array.cpp
@@ -1,6 +1,6 @@
 #include"Array.hpp"
 
-Array::Array(int size):size(size)
+Array::Array(int size) :size(size)
 {
 	data = new int[size];
 	arr.resize(size);
@@ -9,4 +9,10 @@ Array::Array(int size):size(size)
 Array::~Array()
 {
 	delete[] data;
+}
+
+Array::Array(const Array& other) :size(other.size), arr(other.arr)
+{
+	data = new int[size];
+	for (int i = 0; i < size; ++i) data[i] = other.data[i];
 }

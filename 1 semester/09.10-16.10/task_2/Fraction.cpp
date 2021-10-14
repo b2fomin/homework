6c29748f80ed10523d3437c0414e4231ce2e1643
123 <fomin.nv@phystech.edu>
2021-10-14 03:52:23 +0300

    namespace frac

--- 1 semester/09.10-16.10/task_2/Fraction.cpp
+++ 1 semester/09.10-16.10/task_2/Fraction.cpp
@@ -1,181 +1,184 @@
 #include "Fraction.hpp"
 
-Fraction::Fraction(int numerator, unsigned int denominator) :numerator(numerator), denominator(denominator)
+namespace frac
 {
-	reduce_fraction();
-};
-
-int Fraction::gcd(int number_1, int number_2)
-{
-	if (number_2 == 0) return number_1;
-	else return gcd(number_2, number_1 % number_2);
-}
-
-int Fraction::lcm(int number_1, int number_2)
-{
-	return number_1 * number_2 / gcd(number_1, number_2);
-}
-
-void Fraction::reduce_fraction()
-{
-	int divisor = gcd(numerator, denominator);
-	numerator /= divisor;
-	denominator /= divisor;
-}
-
-std::ostream& operator<<(std::ostream& out, const Fraction& other)
-{
-	return out << other.numerator << '/' << other.denominator;
-}
-
-int Fraction::get_numerator() const { return numerator; };
-int Fraction::get_denominator() const { return denominator; };
-
-std::istream& operator>>(std::istream& in, Fraction& other) // input format: "numerator"/"denominator"
-{
-	std::string fraction;
-	in >> fraction;
-	std::string current = "";
-
-	for (auto& elem : fraction)
+	Fraction::Fraction(int numerator, unsigned int denominator) :numerator(numerator), denominator(denominator)
 	{
-		if (elem == '/')
-		{
-			other.numerator = std::stoi(current);
-			current = "";
-		}
+		reduce_fraction();
+	};
+
+	int Fraction::gcd(int number_1, int number_2)
+	{
+		if (number_2 == 0) return number_1;
+		else return gcd(number_2, number_1 % number_2);
 	}
 
-	other.denominator = std::stoi(current);
-	other.reduce_fraction();
+	int Fraction::lcm(int number_1, int number_2)
+	{
+		return number_1 * number_2 / gcd(number_1, number_2);
+	}
 
-	return in;
-}
+	void Fraction::reduce_fraction()
+	{
+		int divisor = gcd(numerator, denominator);
+		numerator /= divisor;
+		denominator /= divisor;
+	}
 
-Fraction operator+(const Fraction& left, const Fraction& right)
-{
-	Fraction new_fraction;
+	std::ostream& operator<<(std::ostream& out, const Fraction& other)
+	{
+		return out << other.numerator << '/' << other.denominator;
+	}
 
-	int dividend = Fraction::lcm(left.denominator, right.denominator);
-	new_fraction.denominator = static_cast<unsigned int>(dividend);
-	new_fraction.numerator = left.numerator * dividend / left.denominator +
-		right.numerator * dividend / left.denominator;
+	int Fraction::get_numerator() const { return numerator; };
+	int Fraction::get_denominator() const { return denominator; };
 
-	new_fraction.reduce_fraction();
-	return new_fraction;
-}
+	std::istream& operator>>(std::istream& in, Fraction& other) // input format: "numerator"/"denominator"
+	{
+		std::string fraction;
+		in >> fraction;
+		std::string current = "";
 
-Fraction operator*(const Fraction& left, const Fraction& right)
-{
-	Fraction new_fraction;
+		for (auto& elem : fraction)
+		{
+			if (elem == '/')
+			{
+				other.numerator = std::stoi(current);
+				current = "";
+			}
+		}
 
-	new_fraction.numerator = left.numerator * right.numerator;
-	new_fraction.denominator = left.denominator * right.denominator;
+		other.denominator = std::stoi(current);
+		other.reduce_fraction();
 
-	new_fraction.reduce_fraction();
-	return new_fraction;
-}
+		return in;
+	}
 
-Fraction operator-(const Fraction& left, const Fraction& right)
-{
-	Fraction new_fraction;
+	Fraction operator+(const Fraction& left, const Fraction& right)
+	{
+		Fraction new_fraction;
 
-	int dividend = Fraction::lcm(left.denominator, right.denominator);
-	new_fraction.denominator = static_cast<unsigned int>(dividend);
-	new_fraction.numerator = left.numerator * dividend / left.denominator -
-		right.numerator * dividend / left.denominator;
+		int dividend = Fraction::lcm(left.denominator, right.denominator);
+		new_fraction.denominator = static_cast<unsigned int>(dividend);
+		new_fraction.numerator = left.numerator * dividend / left.denominator +
+			right.numerator * dividend / left.denominator;
 
-	new_fraction.reduce_fraction();
-	return new_fraction;
-}
+		new_fraction.reduce_fraction();
+		return new_fraction;
+	}
 
-Fraction operator/(const Fraction& left, const Fraction& right)
-{
-	Fraction new_fraction;
+	Fraction operator*(const Fraction& left, const Fraction& right)
+	{
+		Fraction new_fraction;
 
-	new_fraction.numerator = left.numerator * right.denominator;
-	new_fraction.denominator = left.denominator * right.numerator;
+		new_fraction.numerator = left.numerator * right.numerator;
+		new_fraction.denominator = left.denominator * right.denominator;
 
-	new_fraction.reduce_fraction();
-	return new_fraction;
-}
+		new_fraction.reduce_fraction();
+		return new_fraction;
+	}
 
-bool operator==(const Fraction& left, const Fraction& right)
-{
-	return(left.numerator == right.numerator) && (left.denominator == right.denominator);
-}
+	Fraction operator-(const Fraction& left, const Fraction& right)
+	{
+		Fraction new_fraction;
 
-bool operator<(const Fraction& left, const Fraction& right)
-{
-	int divident = Fraction::lcm(left.denominator, right.denominator);
-	return (left.numerator * divident / left.denominator < right.numerator* divident / right.denominator);
-}
+		int dividend = Fraction::lcm(left.denominator, right.denominator);
+		new_fraction.denominator = static_cast<unsigned int>(dividend);
+		new_fraction.numerator = left.numerator * dividend / left.denominator -
+			right.numerator * dividend / left.denominator;
 
-bool operator>(const Fraction& left, const Fraction& right)
-{
-	int divident = Fraction::lcm(left.denominator, right.denominator);
-	return (left.numerator * divident / left.denominator > right.numerator* divident / right.denominator);
-}
+		new_fraction.reduce_fraction();
+		return new_fraction;
+	}
 
-bool operator<=(const Fraction& left, const Fraction& right)
-{
-	int divident = Fraction::lcm(left.denominator, right.denominator);
-	return (left.numerator * divident / left.denominator <= right.numerator* divident / right.denominator);
-}
+	Fraction operator/(const Fraction& left, const Fraction& right)
+	{
+		Fraction new_fraction;
 
-bool operator>=(const Fraction& left, const Fraction& right)
-{
-	int divident = Fraction::lcm(left.denominator, right.denominator);
-	return (left.numerator * divident / left.denominator >= right.numerator* divident / right.denominator);
-}
+		new_fraction.numerator = left.numerator * right.denominator;
+		new_fraction.denominator = left.denominator * right.numerator;
 
-Fraction Fraction::operator+=(const Fraction& other)
-{
-	return operator+(*this, other);
-}
+		new_fraction.reduce_fraction();
+		return new_fraction;
+	}
 
-Fraction Fraction::operator*=(const Fraction& other)
-{
-	return operator*(*this, other);
-}
+	bool operator==(const Fraction& left, const Fraction& right)
+	{
+		return(left.numerator == right.numerator) && (left.denominator == right.denominator);
+	}
 
-Fraction Fraction::operator-=(const Fraction& other)
-{
-	return operator-(*this, other);
-}
+	bool operator<(const Fraction& left, const Fraction& right)
+	{
+		int divident = Fraction::lcm(left.denominator, right.denominator);
+		return (left.numerator * divident / left.denominator < right.numerator* divident / right.denominator);
+	}
 
-Fraction Fraction::operator/=(const Fraction& other)
-{
-	return operator/(*this, other);
-}
+	bool operator>(const Fraction& left, const Fraction& right)
+	{
+		int divident = Fraction::lcm(left.denominator, right.denominator);
+		return (left.numerator * divident / left.denominator > right.numerator * divident / right.denominator);
+	}
 
-Fraction Fraction::operator++()
-{
-	numerator += denominator;
-	return *this;
-}
+	bool operator<=(const Fraction& left, const Fraction& right)
+	{
+		int divident = Fraction::lcm(left.denominator, right.denominator);
+		return (left.numerator * divident / left.denominator <= right.numerator * divident / right.denominator);
+	}
 
-Fraction Fraction::operator--()
-{
-	numerator -= denominator;
-	return *this;
-}
+	bool operator>=(const Fraction& left, const Fraction& right)
+	{
+		int divident = Fraction::lcm(left.denominator, right.denominator);
+		return (left.numerator * divident / left.denominator >= right.numerator * divident / right.denominator);
+	}
 
-Fraction Fraction::operator++(int)
-{
-	Fraction old_fraction{ numerator,denominator };
-	numerator += denominator;
-	return old_fraction;
-}
+	Fraction Fraction::operator+=(const Fraction& other)
+	{
+		return operator+(*this, other);
+	}
 
-Fraction Fraction::operator--(int)
-{
-	Fraction old_fraction{ numerator,denominator };
-	numerator -= denominator;
-	return old_fraction;
-}
+	Fraction Fraction::operator*=(const Fraction& other)
+	{
+		return operator*(*this, other);
+	}
 
-Fraction::operator double()
-{
-	return (double)numerator / denominator;
+	Fraction Fraction::operator-=(const Fraction& other)
+	{
+		return operator-(*this, other);
+	}
+
+	Fraction Fraction::operator/=(const Fraction& other)
+	{
+		return operator/(*this, other);
+	}
+
+	Fraction Fraction::operator++()
+	{
+		numerator += denominator;
+		return *this;
+	}
+
+	Fraction Fraction::operator--()
+	{
+		numerator -= denominator;
+		return *this;
+	}
+
+	Fraction Fraction::operator++(int)
+	{
+		Fraction old_fraction{ numerator,denominator };
+		numerator += denominator;
+		return old_fraction;
+	}
+
+	Fraction Fraction::operator--(int)
+	{
+		Fraction old_fraction{ numerator,denominator };
+		numerator -= denominator;
+		return old_fraction;
+	}
+
+	Fraction::operator double()
+	{
+		return (double)numerator / denominator;
+	}
 }
--- 1 semester/09.10-16.10/task_2/Fraction.hpp
+++ 1 semester/09.10-16.10/task_2/Fraction.hpp
@@ -2,41 +2,44 @@
 #include<iostream>
 #include<string>
 
-class Fraction
+namespace frac
 {
-public:
-	static int gcd(int, int);
-	static int lcm(int, int);
-private:
-	int numerator;
-	unsigned int denominator;
+	class Fraction
+	{
+	public:
+		static int gcd(int, int);
+		static int lcm(int, int);
+	private:
+		int numerator;
+		unsigned int denominator;
 
-	void reduce_fraction();
+		void reduce_fraction();
 
-public:
-	Fraction(int numerator = 0, unsigned int denominator = 1);
+	public:
+		Fraction(int numerator = 0, unsigned int denominator = 1);
 
-	int get_numerator() const;
-	int get_denominator() const;
+		int get_numerator() const;
+		int get_denominator() const;
 
-	operator double();
-	friend std::ostream& operator<<(std::ostream&, const Fraction&);
-	friend std::istream& operator>>(std::istream&, Fraction&);
-	friend Fraction operator+(const Fraction&, const Fraction&);
-	friend Fraction operator*(const Fraction&, const Fraction&);
-	friend Fraction operator-(const Fraction&, const Fraction&);
-	friend Fraction operator/(const Fraction&, const Fraction&);
-	Fraction operator+=(const Fraction&);
-	Fraction operator*=(const Fraction&);
-	Fraction operator-=(const Fraction&);
-	Fraction operator/=(const Fraction&);
-	Fraction operator++();
-	Fraction operator--();
-	Fraction operator++(int);
-	Fraction operator--(int);
-	friend bool operator==(const Fraction&, const Fraction&);
-	friend bool operator>(const Fraction&, const Fraction&);
-	friend bool operator<(const Fraction&, const Fraction&);
-	friend bool operator>=(const Fraction&, const Fraction&);
-	friend bool operator<=(const Fraction&, const Fraction&);
-};
+		operator double();
+		friend std::ostream& operator<<(std::ostream&, const Fraction&);
+		friend std::istream& operator>>(std::istream&, Fraction&);
+		friend Fraction operator+(const Fraction&, const Fraction&);
+		friend Fraction operator*(const Fraction&, const Fraction&);
+		friend Fraction operator-(const Fraction&, const Fraction&);
+		friend Fraction operator/(const Fraction&, const Fraction&);
+		Fraction operator+=(const Fraction&);
+		Fraction operator*=(const Fraction&);
+		Fraction operator-=(const Fraction&);
+		Fraction operator/=(const Fraction&);
+		Fraction operator++();
+		Fraction operator--();
+		Fraction operator++(int);
+		Fraction operator--(int);
+		friend bool operator==(const Fraction&, const Fraction&);
+		friend bool operator>(const Fraction&, const Fraction&);
+		friend bool operator<(const Fraction&, const Fraction&);
+		friend bool operator>=(const Fraction&, const Fraction&);
+		friend bool operator<=(const Fraction&, const Fraction&);
+	};
+}

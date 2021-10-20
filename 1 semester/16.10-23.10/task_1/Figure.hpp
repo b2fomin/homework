e8245e87c54d521ea16882d0c4f5cabd159d6db3
123 <fomin.nv@phystech.edu>
2021-10-21 00:55:40 +0300

    class Quadrangle

--- 1 semester/16.10-23.10/task_1/Figure.hpp
+++ 1 semester/16.10-23.10/task_1/Figure.hpp
@@ -26,3 +26,17 @@ public:
 	virtual double perimetr() override;
 	virtual double area() override;
 };
+
+class Quadrangle :public Polygon
+{
+protected:
+	double side1, side2, side3, side4;
+public:
+	Quadrangle(double, double, double, double);
+	virtual ~Quadrangle();
+	virtual double perimetr() override
+	{
+		return side1 + side2 + side3 + side4;
+	}
+	virtual double area() override = 0;
+};

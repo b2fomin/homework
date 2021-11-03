e058871f7bf91a4936a41c64ec65a04e2802dd23
123 <fomin.nv@phystech.edu>
2021-11-04 02:21:27 +0300

    merge_sort

--- 1 semester/30.10-06.11/task_1.cpp
+++ 1 semester/30.10-06.11/task_1.cpp
@@ -16,3 +16,14 @@ void merge(T(&array)[N], int left, int middle, int right, std::function<bool(T, 
 		else current_1++;
 	}
 }
+
+template<class T, std::size_t N>
+void merge_sort(T(&array)[N], std::function<bool(T, T)> comparator = nullptr, int left = 0, int right = N) // left and right - boundaries of part of array to sort
+{
+	if (comparator == nullptr) comparator = [](T n1, T n2) {return n1 << n2; };
+	if (right - left < 2) return;
+	int middle = (left + right) / 2;
+	merge_sort(array, comparator, left, middle);
+	merge_sort(array, comparator, middle, right);
+	merge(array, left, middle, right, comparator);
+}

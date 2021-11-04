92876f4ee646b532b73dde422e246a7a4831d74b
123 <fomin.nv@phystech.edu>
2021-11-04 03:06:29 +0300

    .hpp file

---
+++ 1 semester/30.10-06.11/task_2/Array.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include<iostream>
+
+template<class T>
+class Array
+{
+private:
+	int capacity;
+	int size;
+	T* arr;
+public:
+	Array();
+	Array(std::initializer_list<T>);
+	Array(int size);
+
+	~Array();
+	Array(const Array<T>&);
+	Array<T>& operator=(const Array<T>&);
+	Array(Array<T>&&);
+	Array<T>& operator=(Array<T>&&);
+
+	void push_back(T);
+	T pop(int);
+	void insert(T, int);
+	void resize(int);
+
+	friend std::ostream& operator<<(std::ostream&, const Array<T>);
+	friend std::istream& operator<<(std::istream&, Array<T>);
+	Array<T> operator+(const Array<T>&, const Array<T>&);
+
+};

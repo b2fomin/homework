ef08f2a2517ee693f43ff39410a53247a38f7ecc
123 <fomin.nv@phystech.edu>
2021-11-11 00:34:16 +0300

    task_3

---
+++ 1 semester/06.11-13.11/task_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+template<typename Function, typename... Args>
+auto call_function(Function function, Args... args)
+{
+	return function(args...);
+}
+
+int main()
+{
+	std::cout << std::boolalpha << call_function([](int n1, int n2) {return n1 < n2; }, 5, 4) << std::endl;
+}

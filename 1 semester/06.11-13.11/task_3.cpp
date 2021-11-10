202962940950ab390767ca7f3ab3c2101f2f97a7
123 <fomin.nv@phystech.edu>
2021-11-11 00:34:51 +0300

    return 0

--- 1 semester/06.11-13.11/task_3.cpp
+++ 1 semester/06.11-13.11/task_3.cpp
@@ -9,4 +9,5 @@ auto call_function(Function function, Args... args)
 int main()
 {
 	std::cout << std::boolalpha << call_function([](int n1, int n2) {return n1 < n2; }, 5, 4) << std::endl;
+	return 0;
 }

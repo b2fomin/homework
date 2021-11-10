78b657ff2d744fc3804142e99923d41ee4bae3c0
123 <fomin.nv@phystech.edu>
2021-11-11 00:17:45 +0300

    recursive realisation

--- 1 semester/06.11-13.11/task_1.cpp
+++ 1 semester/06.11-13.11/task_1.cpp
@@ -8,9 +8,20 @@ auto size1(Args...)
 	return result;
 }
 
+std::size_t size2()
+{
+	return 0;
+}
+
+template<typename First, typename... Args>
+auto size2(First first, Args... args)
+{
+	return sizeof(first) + size2(args...);
+}
 
 int main()
 {
-	std::cout << size1(5, 4, "123");
+	std::cout << size1(5, 4, "123")<<std::endl;
+	std::cout << size2(5, 4, "123");
 	return 0;
 }

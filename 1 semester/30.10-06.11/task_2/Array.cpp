7f9efad4183aa3106c9df1a657b7523d0452a028
123 <fomin.nv@phystech.edu>
2021-11-10 23:04:52 +0300

    templates don't support heading files

--- 1 semester/30.10-06.11/task_2/Array.cpp
+++ 1 semester/30.10-06.11/task_2/Array.cpp
@@ -1,4 +1,50 @@
-#include "Array.hpp"
+#pragma once
+#include<iostream>
+#include<exception>
+
+template<class T>
+class Array
+{
+public:
+	using value_type = T;
+	using iterator = T*;
+	using const_iterator = const T*;
+	using reference = T&;
+	using const_reference = const T&;
+	using size_type = std::size_t;
+
+	Array();
+	Array(std::initializer_list<T>);
+	Array(size_type);
+
+	~Array();
+	Array(const Array<T>&);
+	Array<T>& operator=(const Array<T>&);
+	Array(Array<T>&&);
+	Array<T>& operator=(Array<T>&&);
+
+	void push_back(value_type);
+	T pop(size_type);
+	void insert(value_type, size_type);
+	void resize(size_type);
+
+	size_type size() const noexcept;
+	size_type capacity() const noexcept;
+	const_iterator begin() const noexcept;
+	const_iterator end() const noexcept;
+	iterator begin() noexcept;
+	iterator end() noexcept;
+
+	friend std::ostream& operator<<(std::ostream&, const Array<T>);
+	friend Array<T> operator+(const Array<T>&, const Array<T>&);
+	Array<T>& operator+=(const Array<T>&);
+	reference operator[](size_type);
+	const_reference operator[](size_type) const;
+private:
+	size_type m_capacity;
+	size_type m_size;
+	iterator arr;
+};
 
 template<class T>
 Array<T>::Array() =default;

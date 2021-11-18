520c82c27e5bf94e2d16997bf211ebaea4bf839c
123 <fomin.nv@phystech.edu>
2021-11-18 05:43:27 +0300

    task_3

---
+++ 1 semester/13.11-20.11/task_3.cpp
@@ -0,0 +1,25 @@
+#include<functional>
+
+template<typename T>
+struct decay
+{
+	using type = T;
+};
+
+template<typename Return, typename... Args>
+struct decay<const std::function<Return(Args...)>>
+{
+	using type = std::function<Return(Args...)>*;
+};
+
+template<typename T>
+struct decay<T[]>
+{
+	using type = T*;
+};
+
+template<typename T, std::size_t N>
+struct decay<T[N]>
+{
+	using type = T*;
+};

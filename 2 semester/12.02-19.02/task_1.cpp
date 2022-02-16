19991cda1e36db7be9152a69bc4494fb5ecf4ba1
123 <fomin.nv@phystech.edu>
2022-02-17 01:36:32 +0300

    auto changed capacity and bad_alloc exception

--- 2 semester/12.02-19.02/task_1.cpp
+++ 2 semester/12.02-19.02/task_1.cpp
@@ -9,5 +9,17 @@ int main()
 	arr.reserve(10);
 	std::cout << "Manually changed capacity is " << arr.capacity() << std::endl;
 
+	int old_capacity = arr.capacity();
+
+	while (true)
+	{
+		arr.push_back(1);
+		if (old_capacity != arr.capacity())
+		{
+			std::cout << "Auto changed capacity is " << arr.capacity() << std::endl;
+			old_capacity = arr.capacity();
+		}
+	}
+
 	return 0;
 }

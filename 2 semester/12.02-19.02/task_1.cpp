95a47ebec870ca598e8c5590a13b2b82994c1cf8
123 <fomin.nv@phystech.edu>
2022-02-17 01:36:56 +0300

    comments

--- 2 semester/12.02-19.02/task_1.cpp
+++ 2 semester/12.02-19.02/task_1.cpp
@@ -5,9 +5,11 @@ int main()
 {
 	std::vector<int> arr{ 1,1,1 };
 	std::cout << "Capacity is " << arr.capacity() << std::endl;
+	// При инициализации capacity=size
 
 	arr.reserve(10);
 	std::cout << "Manually changed capacity is " << arr.capacity() << std::endl;
+	// С помощью команды reserve вручную изменятся выделяемая память на величину, указанную в скобках.
 
 	int old_capacity = arr.capacity();
 
@@ -18,6 +20,9 @@ int main()
 		{
 			std::cout << "Auto changed capacity is " << arr.capacity() << std::endl;
 			old_capacity = arr.capacity();
+			// Можно заметить, что количество памяти, выделенное на следующем шаге, в 1.5 раза
+			// больше, чем на предыдущем. В случае нехватки памяти в системе выбрасывается
+			// исключение std::bad_alloc.
 		}
 	}
 

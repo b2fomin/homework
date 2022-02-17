61919a07d4827bbc6390b85dc5e28f73ad11423a
123 <fomin.nv@phystech.edu>
2022-02-17 07:40:19 +0300

    fill_multi_array function

--- 2 semester/12.02-19.02/task_3.cpp
+++ 2 semester/12.02-19.02/task_3.cpp
@@ -27,3 +27,44 @@ auto create_multi_array(std::vector<T2>& vector)
 	arr.resize(dimensions);
 	return arr;
 }
+
+template<typename T1, const int Dimensions, typename T2>
+auto fill_multi_array(std::vector<T2>& vector, boost::multi_array<T1, Dimensions>& multi_arr, int dimension = 0, std::vector<int> position=std::vector<int>(Dimensions))
+{
+
+	for (std::size_t i = 0; i < vector.size(); ++i)
+	{
+		position[dimension] = i;
+		multi_arr(position) = vector[i];
+	}
+	return multi_arr;
+}
+
+template<typename T1, const int Dimensions, typename T2>
+auto fill_multi_array(std::vector<std::vector<T2>>& vector, boost::multi_array<T1, Dimensions>& multi_arr, int dimension=0, std::vector<int> position=std::vector<int>(Dimensions))
+{
+	for (std::size_t i = 0; i < vector.size(); ++i)
+	{
+		position[dimension] = i;
+		fill_multi_array<T1, Dimensions>(vector[i], multi_arr, dimension+1, position);
+	}
+	return multi_arr;
+}
+
+int main()
+{
+	std::vector<std::vector<int>> vector{ {1,2,3}, {4,5,6}, {7,8,9} };
+
+	auto multi_arr = create_multi_array<int, 2>(vector);
+	fill_multi_array<int, 2>(vector, multi_arr);
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			std::cout << multi_arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}

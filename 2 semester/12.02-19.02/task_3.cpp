a69d8ea478e22ddab31ef896145040d70c2654cb
123 <fomin.nv@phystech.edu>
2022-02-17 07:40:05 +0300

    create_multi_array function

--- 2 semester/12.02-19.02/task_3.cpp
+++ 2 semester/12.02-19.02/task_3.cpp
@@ -18,3 +18,12 @@ std::vector<int> calculate_dimensions(std::vector<std::vector<T>>& vector)
 	return dimensions;
 }
 
+template<typename T1, int Dimensions, typename T2>
+auto create_multi_array(std::vector<T2>& vector)
+{
+	std::vector<int> dimensions = calculate_dimensions(vector);
+	std::reverse(dimensions.begin(), dimensions.end());
+	boost::multi_array<T1, Dimensions> arr;
+	arr.resize(dimensions);
+	return arr;
+}

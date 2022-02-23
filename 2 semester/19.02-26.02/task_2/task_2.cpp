875e839e73cde95326b8d4934011127cfac5f4c0
123 <fomin.nv@phystech.edu>
2022-02-24 02:40:56 +0300

    measure collisions

--- 2 semester/19.02-26.02/task_2/task_2.cpp
+++ 2 semester/19.02-26.02/task_2/task_2.cpp
@@ -22,7 +22,31 @@ struct Student
 
 int main()
 {
-	
+	char symbols[27] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int size = 1000; size < 200000; size += 10000)
+	{
+		std::size_t collisions=0;
+		std::unordered_set<std::size_t> hashes;
+
+		for (int i = 0; i < size; ++i)
+		{
+			std::string name;
+			for (int j = 0; j < 3; ++j)
+			{
+				name += symbols[std::rand() % 26];
+			}
+
+			Student student{ i % 60, name, (float)(std::rand() % 10) / 2 };
+			auto old_size = hashes.size();
+			hashes.insert(hash_value(student));
+			
+			if (old_size == hashes.size())
+				++collisions;
+		}
+
+		std::cout << size << " " << collisions << std::endl;
+	}
 
 	return 0;
 }

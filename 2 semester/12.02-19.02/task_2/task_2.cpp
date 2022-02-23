5fd2b4d8a72d35d843813307a5e7b618d8d50113
123 <fomin.nv@phystech.edu>
2022-02-23 23:18:41 +0300

    separate Stopwatch class

--- 2 semester/12.02-19.02/task_2/task_2.cpp
+++ 2 semester/12.02-19.02/task_2/task_2.cpp
@@ -1,63 +1,11 @@
 #include<iostream>
-#include<chrono>
+#include"Stopwatch.hpp"
 #include<algorithm>
 #include<vector>
 #include<deque>
 #include<array>
 #include<list>
 #include<forward_list>
-
-class Stopwatch
-{
-private:
-	using timepoint = std::chrono::steady_clock::time_point;
-	timepoint startpoint;
-	timepoint stoppoint;
-	bool is_stopped;
-
-public:
-	Stopwatch();
-	void start();
-	void stop();
-	void reset();
-	template<typename ratio=std::chrono::microseconds>
-	int time();
-};
-
-Stopwatch::Stopwatch() : is_stopped{ true }
-{}
-
-void Stopwatch::start()
-{
-	if (is_stopped)
-	{
-		is_stopped = false;
-		startpoint = std::chrono::steady_clock::now() - (stoppoint - startpoint);
-	}
-}
-
-void Stopwatch::stop()
-{
-	if (!is_stopped)
-	{
-		is_stopped = true;
-		stoppoint = std::chrono::steady_clock::now();
-	}
-}
-
-void Stopwatch::reset()
-{
-	is_stopped = true;
-	startpoint = stoppoint = std::chrono::steady_clock::now();
-}
-
-template<typename ratio>
-int Stopwatch::time()
-{
-	if (!is_stopped) stoppoint = std::chrono::steady_clock::now();
-
-	return std::chrono::duration_cast<ratio>(stoppoint - startpoint).count();
-}
 
 int main()
 {

a27be96e85fa6536caa68b47aed4a0f905b80339
123 <fomin.nv@phystech.edu>
2022-02-23 23:39:58 +0300

    vector filling

--- 2 semester/19.02-26.02/task_1/main.cpp
+++ 2 semester/19.02-26.02/task_1/main.cpp
@@ -2,21 +2,35 @@
 #include"Stopwatch.hpp"
 #include<vector>
 #include<set>
+#include<algorithm>
 
 int main()
 {
 	std::set<int> set;
 	std::vector<int> vector;
 	Stopwatch mywatch;
-	
+
 	mywatch.start();
 	for (int i = 0; i < 100000; ++i)
 	{
 		set.insert(std::rand());
 	}
 
-	std::cout << "Set time is " << mywatch.time() << std::endl;
+	mywatch.stop();
+	std::cout << "Set's time is " << mywatch.time() << std::endl;
 	mywatch.reset();
+
+	for (auto& elem : set)
+	{
+		vector.push_back(elem);
+	}
+
+	mywatch.start();
+	std::sort(vector.begin(), vector.end());
+
+	mywatch.stop();
+	std::cout << "Vector's time is " << mywatch.time() << std::endl;
+
 
 	return 0;
 }

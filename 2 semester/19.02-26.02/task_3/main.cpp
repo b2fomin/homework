611fba61870636de1418a95c5f0507b2f92e648a
123 <fomin.nv@phystech.edu>
2022-03-03 01:16:20 +0300

    strings generate only one time and they are no duplicates

--- 2 semester/19.02-26.02/task_3/main.cpp
+++ 2 semester/19.02-26.02/task_3/main.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 #include<unordered_set>
 #include"Hash_functions.hpp"
-
+#include<vector>
 
 int main()
 {
 	char symbols[27] = "abcdefghijklmnopqrstuvwxyz";
+	std::vector<char*> strings;
+
+	for (int i = 0; i < 5000000; ++i)
+	{
+		char* string = new char[6];
+		int i_copy = i;
+		for (int j = 0; j < 6; ++j)
+		{
+			string[j] = symbols[i_copy % 27];
+			i_copy /= 27;
+		}
+
+		strings.push_back(string);
+	}
 
 	for (auto& function : {RSHash, JSHash, PJWHash, ELFHash,
 		BKDRHash, SDBMHash, DJBHash, DEKHash, APHash})
@@ -13,25 +27,20 @@ int main()
 		std::size_t collisions = 0;
 		std::unordered_set<std::size_t> hashes;
 
-		for (int i = 0; i < 326797; ++i)
+		for (auto& string:strings)
 		{
-			char* string=new char[6];
-			for (int j = 0; j < 6; ++j)
-			{
-				string[j] = symbols[std::rand() % 26];
-			}
-
 			auto old_size = hashes.size();
 			hashes.insert(function(string, 6));
 
 			if (old_size == hashes.size())
 				++collisions;
-			
-			delete[] string;
 		}
 
 		std::cout << collisions << std::endl;
 	}
+
+	for (auto& string : strings)
+		delete[] string;
 
 	return 0;
 }

521590acc1a85c6569a0f8c8d8fdc63b0f848331
123 <fomin.nv@phystech.edu>
2022-03-10 01:32:55 +0300

    task 7

--- 2 semester/05.03-12.03/tasks.cpp
+++ 2 semester/05.03-12.03/tasks.cpp
@@ -18,15 +18,15 @@ int main()
 	std::vector<int> arr;
 	for (int i = 1; i <= 10; ++i)
 		arr.push_back(i);
-	
+
 	print_vector(arr);
 
-	while(std::cin.peek()!='\n')
+	while (std::cin.peek() != '\n')
 	{
 		int number;
 		std::cin >> number;
 		arr.push_back(number);
-		
+
 	}
 
 	print_vector(arr);
@@ -40,14 +40,26 @@ int main()
 	arr.erase(std::unique(arr.begin(), arr.end()), arr.end());// can be deleted
 	for (int i = 0; i < arr.size(); ++i)
 	{
-		arr.erase(std::remove(arr.begin() + i+1, arr.end(), arr[i]), arr.end());
+		arr.erase(std::remove(arr.begin() + i + 1, arr.end(), arr[i]), arr.end());
 	}
 
 	print_vector(arr);
 
 	std::cout << std::count_if(arr.begin(), arr.end(), [](const auto& elem) {return elem % 2; }) << std::endl;
 
-	std::cout << "min: " << *std::min_element(arr.begin(), arr.end()) << ", max: " << *std::max_element(arr.begin(), arr.end());
+	std::cout << "min: " << *std::min_element(arr.begin(), arr.end()) << ", max: " << *std::max_element(arr.begin(), arr.end()) << std::endl;
+
+	std::cout << *std::find_if(arr.begin(), arr.end(), [](const auto& elem)
+		{
+			if (elem <= 1)
+				return false;
+
+			for (int i = 2; i * i <= elem; ++i)
+				if (elem % i == 0)
+					return false;
+
+			return true;
+		}) << std::endl;
 
 	return 0;
 }

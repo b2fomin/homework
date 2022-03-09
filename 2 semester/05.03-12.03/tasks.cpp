5a3fca5ada0da223ee850a3bde53b1ccdff489fd
123 <fomin.nv@phystech.edu>
2022-03-10 01:27:27 +0300

    function print_vector to show changes in vector

--- 2 semester/05.03-12.03/tasks.cpp
+++ 2 semester/05.03-12.03/tasks.cpp
@@ -4,11 +4,22 @@
 #include<algorithm>
 #include<random>
 
+template<typename T>
+void print_vector(std::vector<T> arr)
+{
+	for (auto& elem : arr)
+		std::cout << elem << " ";
+
+	std::cout << std::endl;
+}
+
 int main()
 {
 	std::vector<int> arr;
 	for (int i = 1; i <= 10; ++i)
 		arr.push_back(i);
+	
+	print_vector(arr);
 
 	while(std::cin.peek()!='\n')
 	{
@@ -18,15 +29,21 @@ int main()
 		
 	}
 
+	print_vector(arr);
+
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::shuffle(arr.begin(), arr.end(), gen);
+
+	print_vector(arr);
 
 	arr.erase(std::unique(arr.begin(), arr.end()), arr.end());// can be deleted
 	for (int i = 0; i < arr.size(); ++i)
 	{
 		arr.erase(std::remove(arr.begin() + i+1, arr.end(), arr[i]), arr.end());
 	}
+
+	print_vector(arr);
 
 	std::cout << std::count_if(arr.begin(), arr.end(), [](const auto& elem) {return elem % 2; }) << std::endl;
 

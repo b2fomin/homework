4a1e137bc2b0b705258842fdf9510be1165f7bf0
123 <fomin.nv@phystech.edu>
2022-03-10 00:05:31 +0300

    task 2

--- 2 semester/05.03-12.03/tasks.cpp
+++ 2 semester/05.03-12.03/tasks.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 int main()
 {
 	std::vector<int> arr;
-	for (int i = 0; i < 10; ++i)
+	for (int i = 1; i <= 10; ++i)
 		arr.push_back(i);
+
+	while(std::cin.peek()!='\n')
+	{
+		int number;
+		std::cin >> number;
+		arr.push_back(number);
+		
+	}
 
 	return 0;
 }

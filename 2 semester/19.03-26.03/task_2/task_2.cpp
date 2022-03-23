98fc5878fc374336f93375f9df245be213d3332a
123 <fomin.nv@phystech.edu>
2022-03-24 02:33:57 +0300

    finding n's row

--- 2 semester/19.03-26.03/task_2/task_2.cpp
+++ 2 semester/19.03-26.03/task_2/task_2.cpp
@@ -21,5 +21,13 @@ int main()
 		file << std::endl;
 	}
 
+	file.close();
+	file.open("./task_2/task_2.txt", std::ios_base::in);
+	std::size_t number;
+	std::cin >> number;
+	file.seekg((number - 1) * (size_of_line+2));
+	for (int i = 0; i < size_of_line + 1; ++i)
+		std::cout << (char)(file.get());
+
 	return 0;
 }

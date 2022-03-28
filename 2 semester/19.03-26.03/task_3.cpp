0c26c682f204de2e04be9a6b9ea1fb9e0e76cd1e
123 <fomin.nv@phystech.edu>
2022-03-28 07:58:45 +0300

    corrections in task 3

--- 2 semester/19.03-26.03/task_3.cpp
+++ 2 semester/19.03-26.03/task_3.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<filesystem>
 #include<string>
+#include<chrono>
 
 int main()
 {
@@ -13,14 +14,28 @@ int main()
 	{
 		for (auto const& entry : std::filesystem::directory_iterator(path))
 		{
-			std::cout << "File's name: " << entry.path().filename().string() << std::endl;
-
 			if (std::filesystem::is_regular_file(entry.status()) || std::filesystem::is_symlink(entry.status()))
 			{
+				std::string filename = entry.path().filename().string();
+				auto point = filename.rfind('.');
+
+				std::cout << "File's name: ";
+				for (std::size_t i = 0; i < point; ++i) std::cout << filename[i];
+
+				std::cout << std::endl << "File's extension: ";
+				for (std::size_t i = point; i < filename.size(); ++i) std::cout << filename[i];
+				std::cout << std::endl;
+
 				std::cout << "File's size: " << std::filesystem::file_size(entry) << std::endl;
 			}
+			else if (std::filesystem::is_directory(path))
+					std::cout << "Directory's name: " << entry.path().filename().string() << std::endl;
 
-			std::cout << "File's last write time: " << std::chrono::duration_cast<std::chrono::seconds>(std::filesystem::last_write_time(entry).time_since_epoch()).count()<<std::endl;
+			
+			auto time = std::chrono::system_clock::time_point{ std::filesystem::last_write_time(entry).time_since_epoch() };
+			auto time_t = std::chrono::system_clock::to_time_t(time);
+
+			std::cout << "Last write time: " << std::put_time(std::localtime(&time_t), "%Y-%m-%d %X")<<std::endl;
 		}
 
 		std::cout << std::endl;

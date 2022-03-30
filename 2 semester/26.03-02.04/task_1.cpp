9018eb03777de67359f98c1559ff7044e16eb92f
123 <fomin.nv@phystech.edu>
2022-03-31 01:05:03 +0300

    function parallel_integrate

--- 2 semester/26.03-02.04/task_1.cpp
+++ 2 semester/26.03-02.04/task_1.cpp
@@ -16,6 +16,33 @@ double integrate(double (*func)(double), double a, double b, double eps = pow(10
 	return res;
 }
 
+double parallel_integrate(double (*func)(double), double a, double b, double eps = pow(10, -6))
+{
+	const std::size_t max_threads = (int)((b - a + eps) / eps);
+	const std::size_t hardware_threads = std::thread::hardware_concurrency();
+	const std::size_t num_threads = std::min(hardware_threads == 0 ? 2 : hardware_threads, max_threads);
+
+	std::vector<std::future<double>> futures(num_threads - 1);
+	double left = a, right = a + (b - a) / num_threads;
+
+	for (int i = 0; i < num_threads - 1; ++i)
+	{
+		futures[i] = std::async(integrate, func, left, right, eps);
+
+		left += (b - a) / num_threads;
+		right += (b - a) / num_threads;
+	}
+
+	auto result = integrate(func, left, right, eps);
+
+	for (auto& future : futures)
+	{
+		result += future.get();
+	}
+
+	return result;
+}
+
 int main()
 {
 	return 0;

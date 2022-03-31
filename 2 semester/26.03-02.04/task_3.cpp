1779e965eff6a6b74d044d27c6838841d827bb95
123 <fomin.nv@phystech.edu>
2022-03-31 05:39:42 +0300

    5th question

--- 2 semester/26.03-02.04/task_3.cpp
+++ 2 semester/26.03-02.04/task_3.cpp
@@ -3,47 +3,36 @@
 #include<thread>
 #include<future>
 #include<vector>
-#include<memory>
+#include<execution>
 
 template<typename Iterator, typename Func>
 Func parallel_for_each(Iterator first, Iterator last, Func func)
 {
 	const std::size_t length = std::distance(first, last);
 	const std::size_t min_per_thread = 25;
-	const std::size_t max_size = (length + min_per_thread - 1) / min_per_thread;
+	const std::size_t max_threads = (length + min_per_thread - 1) / min_per_thread;
 	const std::size_t hardware_threads = std::thread::hardware_concurrency();
-	const std::size_t num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_size);
-	std::vector<std::future<void>> futures(num_threads - 1);
+	const std::size_t num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
 
-	std::size_t block_size = length / num_threads;
-	auto left = first;
-	auto right = first;
-	std::advance(right, block_size);
+	if (num_threads <= 2) return std::for_each(first, last, func);
 
-	for (auto& future : futures)
-	{
-		future = std::async([](auto left, auto right, auto func) {std::for_each(left, right, func); }, left, right, func);
+	Iterator middle = first;
+	std::advance(middle, length / 2);
+	std::future<void> first_half_result = std::async(
+		[](auto first, auto last, auto func) {parallel_for_each(first, last, func); }, first, middle, func);
 
-		std::advance(left, block_size);
-		std::advance(right, block_size);
-	}
-
-	std::for_each(left, right, func);
-
-	for (auto& future : futures)
-	{
-		future.get();
-	}
+	parallel_for_each(middle, last, func);
+	first_half_result.get();
 
 	return func;
 }
 
 int main()
 {
-	std::vector<int> data(10);
-	std::fill(data.begin(), data.end(), 2);
+	std::vector<long long int> data(pow(10, 5));
+	for (std::size_t i = 0; i < data.size(); ++i) data[i] = i;
 
-	auto func = [](int elem) { std::cout << elem*elem << " "; };
+	auto func = [](auto& elem) { elem=elem*elem; };
 
 	parallel_for_each(data.begin(), data.end(), func);
 

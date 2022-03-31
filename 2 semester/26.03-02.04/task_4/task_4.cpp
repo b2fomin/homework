5a5f6c209a655c7b3e612b3805fdc5b05f6bf25c
123 <fomin.nv@phystech.edu>
2022-03-31 05:05:42 +0300

    sort

--- 2 semester/26.03-02.04/task_4/task_4.cpp
+++ 2 semester/26.03-02.04/task_4/task_4.cpp
@@ -24,5 +24,19 @@ int main()
 	std::cout << "not_parallel_for_each: " << mywatch.time() << std::endl;
 	mywatch.reset();
 
+	std::cout << std::endl;
+
+	mywatch.start();
+	std::sort(std::execution::par, data.begin(), data.end());
+	mywatch.stop();
+	std::cout << "parallel_sort: " << mywatch.time() << std::endl;
+	mywatch.reset();
+
+	mywatch.start();
+	std::sort(std::execution::seq, data.begin(), data.end());
+	mywatch.stop();
+	std::cout << "not_parallel_sort: " << mywatch.time() << std::endl;
+	mywatch.reset();
+
 	return 0;
 }

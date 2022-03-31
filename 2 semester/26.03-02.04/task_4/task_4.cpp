59a4b9bd9de7b9031154a42334056c72bd251366
123 <fomin.nv@phystech.edu>
2022-03-31 05:05:26 +0300

    for_each

---
+++ 2 semester/26.03-02.04/task_4/task_4.cpp
@@ -0,0 +1,28 @@
+#include<algorithm>
+#include<execution>
+#include<iostream>
+#include<vector>
+#include"Stopwatch.hpp"
+
+int main()
+{
+	Stopwatch mywatch;
+	std::vector<int> data(pow(10, 6));
+	for (int i = 0; i < data.size(); ++i) data[i] = i;
+
+	auto func = [](int& elem) {return elem * elem; };
+
+	mywatch.start();
+	std::for_each(std::execution::par, data.begin(), data.end(), func);
+	mywatch.stop();
+	std::cout << "parallel_for_each: " << mywatch.time() << std::endl;
+	mywatch.reset();
+
+	mywatch.start();
+	std::for_each(std::execution::seq, data.begin(), data.end(), func);
+	mywatch.stop();
+	std::cout << "not_parallel_for_each: " << mywatch.time() << std::endl;
+	mywatch.reset();
+
+	return 0;
+}

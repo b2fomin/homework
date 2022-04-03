0e540d36a29ee5765c4a9d070fa0dee7f2f654a8
123 <fomin.nv@phystech.edu>
2022-04-03 22:00:03 +0300

    unlimited increasing of number of threads

--- 2 semester/26.03-02.04/task_2/task_2.cpp
+++ 2 semester/26.03-02.04/task_2/task_2.cpp
@@ -14,7 +14,7 @@ T parallel_accumulate(Iterator first, Iterator last, T init, Func func = std::pl
 	const std::size_t min_per_thread = 25;
 	const std::size_t max_size = (length + min_per_thread - 1) / min_per_thread;
 	const std::size_t hardware_threads = std::thread::hardware_concurrency();
-	const std::size_t num_threads = std::min(std::min(hardware_threads != 0 ? hardware_threads : 2, max_size), std::min(max_size, wanted_num_threads));
+	const std::size_t num_threads = wanted_num_threads;
 
 	std::vector<std::future<T>> futures(num_threads - 1);
 	const std::size_t block_size = length / num_threads;
@@ -43,10 +43,10 @@ T parallel_accumulate(Iterator first, Iterator last, T init, Func func = std::pl
 int main()
 {
 	Stopwatch mywatch;
-	std::vector<long long int> data(pow(10,8));
+	std::vector<long long int> data(pow(10,6));
 	std::iota(data.begin(), data.end(), 2);
 
-	for (std::size_t num_threads = 1; num_threads <= std::thread::hardware_concurrency(); ++num_threads)
+	for (std::size_t num_threads = 1; num_threads <= std::thread::hardware_concurrency()+1000; ++num_threads)
 	{
 		mywatch.start();
 		parallel_accumulate(data.begin(), data.end(), 1, std::multiplies<long long int>(), num_threads);
@@ -58,4 +58,5 @@ int main()
 /*
 * По графику видно, что время убывает с возрастанием количества потоков. При N>=2 производительность увеличивается не более чем в 1.2 раза.
   Оптимальным можно считать N=5, так как время находится в пределах 30-40 мс при N>=5.
+  При неограниченном же возрастании количества потоков переключение между ними занимает время, сравнимое или больше со временем выполнения задачи в потоке.
 */

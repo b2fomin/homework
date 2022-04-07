01542d5da882aab75caa73ca9f4e51396ce292c9
123 <fomin.nv@phystech.edu>
2022-04-07 05:11:52 +0300

    push, pop and swap methods

--- 2 semester/02.04-09.04/task_3/priority_queue.hpp
+++ 2 semester/02.04-09.04/task_3/priority_queue.hpp
@@ -23,6 +23,10 @@ public:
 	const value_type& top() const noexcept;
 	bool empty() const noexcept;
 	size_type size() const noexcept;
+	void push(value_type&&);
+	void wait_and_pop();
+	bool try_pop();
+	void swap(reference) noexcept;
 };
 
 template<typename T>
@@ -58,4 +62,38 @@ typename Priority_queue<T>::size_type Priority_queue<T>::size() const noexcept
 {
 	std::lock_guard<std::mutex> lock(m_mutex);
 	return m_prior_queue.size();
+}
+
+template<typename T>
+void Priority_queue<T>::push(T&& value)
+{
+	std::lock_guard<std::mutex> lock(m_mutex);
+	m_prior_queue.push(value);
+	m_condition_variable.notify_one();
+}
+
+template<typename T>
+void Priority_queue<T>::wait_and_pop()
+{
+	std::unique_lock<std::mutex> lock(m_mutex);
+	m_condition_variable.wait(lock, [this] {return !m_prior_queue.empty(); });
+	m_prior_queue.pop();
+}
+
+template<typename T>
+bool Priority_queue<T>::try_pop()
+{
+	std::lock_guard<std::mutex> lock(m_mutex);
+
+	if (m_prior_queue.empty()) return false;
+
+	m_prior_queue.pop();
+	return true;
+}
+
+template<typename T>
+void Priority_queue<T>::swap(reference other) noexcept
+{
+	std::scoped_lock lock(m_mutex, other.m_mutex);
+	m_prior_queue.swap(other);
 }

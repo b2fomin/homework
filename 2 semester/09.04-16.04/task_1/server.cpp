1f82636275132124e12d0ff8596cc7c0bb25b42e
123 <fomin.nv@phystech.edu>
2022-04-14 08:11:26 +0300

    vector instead of pair

--- 2 semester/09.04-16.04/task_1/server.cpp
+++ 2 semester/09.04-16.04/task_1/server.cpp
@@ -1,18 +1,20 @@
 #define BOOST_DATE_TIME_NO_LIB
 
 #include<boost/interprocess/managed_shared_memory.hpp>
-#include<boost/interprocess/containers/map.hpp>
+#include<boost/interprocess/containers/vector.hpp>
+#include<boost/interprocess/containers/string.hpp>
 #include<boost/interprocess/allocators/allocator.hpp>
 #include<string>
 
 int main()
 {
-
-	using value_type = std::pair<const std::string, std::string>;
-	using allocator = boost::interprocess::allocator<value_type,
-		boost::interprocess::managed_shared_memory::segment_manager>;
-	using My_map = boost::interprocess::map<std::string, std::string,
-		std::less<std::string>, allocator>;
+	using segment_manager_t = boost::interprocess::managed_shared_memory::segment_manager;
+	using char_allocator = boost::interprocess::allocator<char, segment_manager_t>;
+	using char_string = boost::interprocess::basic_string<char, std::char_traits<char>, char_allocator>;
+	using value_allocator = boost::interprocess::allocator<char_string, segment_manager_t>;
+	using value_type = std::vector<char_string, value_allocator>;
+	using allocator = boost::interprocess::allocator<value_type, segment_manager_t>;
+	using My_vector = boost::interprocess::vector<value_type, allocator>;
 
 	const std::size_t memory_size = 1024;
 	const std::string memory_name = "shared_memory";
@@ -21,8 +23,11 @@ int main()
 	boost::interprocess::managed_shared_memory shared_memory(
 	boost::interprocess::create_only, memory_name.c_str(), memory_size);
 
-	My_map* messages = shared_memory.construct<My_map>("messages")
-		(std::less<std::string>(), shared_memory.get_segment_manager());
+	My_vector* messages = shared_memory.construct<My_vector>("messages")
+		(shared_memory.get_segment_manager());
+
+	int* number_of_programs = shared_memory.construct<int>("number_of_programs")(0);
+	auto input_somewhere = shared_memory.construct<bool>("input_somewhere")(false);
 
 	system("pause");
 

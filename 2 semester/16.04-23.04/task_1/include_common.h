6e199b1ccba15e3d7d4b9967ebca280cd1edc7f8
123 <fomin.nv@phystech.edu>
2022-04-22 07:40:46 +0300

    include_common.h

---
+++ 2 semester/16.04-23.04/task_1/include_common.h
@@ -0,0 +1,49 @@
+#pragma once
+#pragma warning(disable:4996)
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#include<boost/asio.hpp>
+#include<boost/asio/ssl.hpp>
+#include<boost/property_tree/ptree.hpp>
+#include<boost/property_tree/json_parser.hpp>
+
+#include<iostream>
+#include"tsqueue.h"
+#include<string>
+#include<memory>
+#include<sstream>
+#include<regex>
+#include<chrono>
+#include<format>
+#include<vector>
+#include<algorithm>
+#include<filesystem>
+
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+namespace ssl = net::ssl;
+using err_code = boost::system::error_code;
+namespace pt = boost::property_tree;
+
+void fail(const err_code ec, const char* what)
+{
+	std::cout << what << ": " << ec.message() << std::endl;
+}
+
+enum class ServerCommand
+{
+	Message,
+	NoMessage
+};
+
+enum class ClientCommand
+{
+	LogIn,
+	Register,
+	Chat,
+	Message
+};
+
+const std::string delimeter = "#";
